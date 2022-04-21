dee8e6832c3a4b793ab9570307b35e4d52429482
Miguel <mr.baiao@campus.fct.unl.pt>
2022-04-21 16:33:39 +0100

    ate teste 7b

--- src/tp1/server/REST/resources/UsersResource.java
+++ src/tp1/server/REST/resources/UsersResource.java
@@ -19,183 +19,167 @@ import tp1.clients.REST.RestDirClient;
 @Singleton
 public class UsersResource implements RestUsers {
 
-	public final Map<String, User> users = new HashMap<>();
+    public final Map<String, User> users = new HashMap<>();
 
-	private static Logger Log = Logger.getLogger(UsersResource.class.getName());
-	private  RestDirClient dirClient;
+    private static Logger Log = Logger.getLogger(UsersResource.class.getName());
 
-	private Discovery discv = new Discovery(null, "users", null);
+    private Discovery discv = new Discovery(null, "users", null);
 
-	public UsersResource() {
-		discv.listener();
-		dirClient = new RestDirClient(getServiceURI("directory"));
-
-	}
-
-	@Override
-	public String createUser(User user) {
-		Log.info("createUser : " + user);
-
-		// Check if user data is valid
-		if(user.getUserId() == null || user.getPassword() == null || user.getFullName() == null ||
-				user.getEmail() == null) {
-			Log.info("User object invalid.");
-			throw new WebApplicationException( Status.BAD_REQUEST );
-		}
-
-		// Check if userId already exists
-		if(users.containsKey(user.getUserId())) {
-			Log.info("User already exists.");
-			throw new WebApplicationException( Status.CONFLICT );
-		}
-
-		//Add the user to the map of users
-		users.put(user.getUserId(), user);
-		return user.getUserId();
-	}
+    public UsersResource() {
 
 
-	@Override
-	public User getUser(String userId, String password) {
-		Log.info("getUser : user = " + userId + "; pwd = " + password);
 
-		User user = users.get(userId);
+    }
 
-		// Check if user exists
-		if( user == null ) {
-			Log.info("User does not exist.");
-			throw new WebApplicationException( Status.NOT_FOUND );
-		}
+    @Override
+    public String createUser(User user) {
+        Log.info("createUser : " + user);
 
-		// Check if user is valid
-		if(userId == null || password == null) {
-			Log.info("UserId or password null.");
-			throw new WebApplicationException( Status.FORBIDDEN );
-		}
+        // Check if user data is valid
+        if (user.getUserId() == null || user.getPassword() == null || user.getFullName() == null ||
+                user.getEmail() == null) {
+            Log.info("User object invalid.");
+            throw new WebApplicationException(Status.BAD_REQUEST);
+        }
 
-		//Check if the password is correct
-		if( !user.getPassword().equals( password)) {
-			Log.info("Password is incorrect.");
-			throw new WebApplicationException( Status.FORBIDDEN );
-		}
+        // Check if userId already exists
+        if (users.containsKey(user.getUserId())) {
+            Log.info("User already exists.");
+            throw new WebApplicationException(Status.CONFLICT);
+        }
 
-		return user;
-	}
-
-/*
-	@Override
-	public int getUserbyId(String userId){
-
-		User user = users.get(userId);
-
-		// Check if user exists
-		if( user == null ) {
-			Log.info("User does not exist.");
-			throw new WebApplicationException( Status.NOT_FOUND );
-		}
-
-		return user;
-	}
-*/
-
-	@Override
-	public User updateUser(String userId, String password, User updatedUser) {
-		Log.info("updateUser : user = " + userId + "; pwd = " + password + " ; updatedUser = " + updatedUser);
-
-		// Check if user is valid
-		if(userId == null || password == null) {
-			Log.info("UserId or password null.");
-			throw new WebApplicationException( Status.FORBIDDEN );
-		}
-
-		User user = users.get(userId);
-
-		// Check if user exists
-		if( user == null ) {
-			Log.info("User does not exist.");
-			throw new WebApplicationException( Status.NOT_FOUND );
-		}
-
-		//Check if the password is correct
-		if( !user.getPassword().equals(password)) {
-			Log.info("Password is incorrect.");
-			throw new WebApplicationException( Status.FORBIDDEN );
-		}
-
-		if(updatedUser.getUserId() == userId || updatedUser.getUserId() == null) {
-
-			if (updatedUser.getPassword() != null)
-				user.setPassword(updatedUser.getPassword());
-			if (updatedUser.getEmail() != null)
-				user.setEmail(updatedUser.getEmail());
-			if (updatedUser.getFullName() != null)
-				user.setFullName(updatedUser.getFullName());
-		}
-		return users.get(userId);
-	}
+        //Add the user to the map of users
+        users.put(user.getUserId(), user);
+        return user.getUserId();
+    }
 
 
-	public User deleteUser(String userId, String password) {
-		Log.info("deleteUser : user = " + userId + "; pwd = " + password);
-		//To Test
+    @Override
+    public User getUser(String userId, String password) {
+        Log.info("getUser : user = " + userId + "; pwd = " + password);
 
-		// Check if user is valid
-		if(userId == null || password == null) {
-			Log.info("UserId or password null.");
-			throw new WebApplicationException( Status.FORBIDDEN );
-		}
+        User user = users.get(userId);
 
-		User user = users.get(userId);
+        // Check if user exists
+        if (user == null) {
+            Log.info("User does not exist.");
+            throw new WebApplicationException(Status.NOT_FOUND);
+        }
 
-		// Check if user exists
-		if( user == null ) {
-			Log.info("User does not exist.");
-			throw new WebApplicationException( Status.NOT_FOUND );
-		}
+        // Check if user is valid
+        if (userId == null || password == null) {
+            Log.info("UserId or password null.");
+            throw new WebApplicationException(Status.FORBIDDEN);
+        }
 
-		//Check if the password is correct
-		if( !user.getPassword().equals( password)) {
-			Log.info("Password is incorrect.");
-			throw new WebApplicationException( Status.FORBIDDEN );
-		}
-		users.remove(userId);
-		dirClient.deleteUserFiles(userId,password);
+        //Check if the password is correct
+        if (!user.getPassword().equals(password)) {
+            Log.info("Password is incorrect.");
+            throw new WebApplicationException(Status.FORBIDDEN);
+        }
 
-
-		return user;
-	}
+        return user;
+    }
 
 
-	@Override
-	public List<User> searchUsers(String pattern) {
-		Log.info("searchUsers : pattern = " + pattern);
+    @Override
+    public User updateUser(String userId, String password, User updatedUser) {
+        Log.info("updateUser : user = " + userId + "; pwd = " + password + " ; updatedUser = " + updatedUser);
 
-		List<User> res = new ArrayList<>();
-		String LowerPattern = pattern.toLowerCase();
-		for (Map.Entry<String,User> set: users.entrySet()) {
-			if(set.getValue().getFullName().toLowerCase().contains(LowerPattern)) {
-				res.add(set.getValue());
-			}
-		}
+        // Check if user is valid
+        if (userId == null || password == null) {
+            Log.info("UserId or password null.");
+            throw new WebApplicationException(Status.FORBIDDEN);
+        }
 
-		return res;
-	}
+        User user = users.get(userId);
 
-	private URI getServiceURI(String serviceName) {
+        // Check if user exists
+        if (user == null) {
+            Log.info("User does not exist.");
+            throw new WebApplicationException(Status.NOT_FOUND);
+        }
 
-		URI uri = null;
-		discv.listener();
-		while (true) {
-			if (discv.knownUrisOf(serviceName).length > 0) {
-				uri = discv.knownUrisOf(serviceName)[0];
-				break;
-			}
-		}
+        //Check if the password is correct
+        if (!user.getPassword().equals(password)) {
+            Log.info("Password is incorrect.");
+            throw new WebApplicationException(Status.FORBIDDEN);
+        }
 
-		return uri;
+        if (updatedUser.getUserId() == userId || updatedUser.getUserId() == null) {
+
+            if (updatedUser.getPassword() != null)
+                user.setPassword(updatedUser.getPassword());
+            if (updatedUser.getEmail() != null)
+                user.setEmail(updatedUser.getEmail());
+            if (updatedUser.getFullName() != null)
+                user.setFullName(updatedUser.getFullName());
+        }
+        return users.get(userId);
+    }
 
 
-	}
+    public User deleteUser(String userId, String password) {
+        Log.info("deleteUser : user = " + userId + "; pwd = " + password);
+        //To Test
+
+        // Check if user is valid
+        if (userId == null || password == null) {
+            Log.info("UserId or password null.");
+            throw new WebApplicationException(Status.FORBIDDEN);
+        }
+
+        User user = users.get(userId);
+
+        // Check if user exists
+        if (user == null) {
+            Log.info("User does not exist.");
+            throw new WebApplicationException(Status.NOT_FOUND);
+        }
+
+        //Check if the password is correct
+        if (!user.getPassword().equals(password)) {
+            Log.info("Password is incorrect.");
+            throw new WebApplicationException(Status.FORBIDDEN);
+        }
+        users.remove(userId);
+        RestDirClient dirClient = new RestDirClient(getServiceURI("directory"));
+        if(dirClient != null)
+        dirClient.deleteUserFiles(userId, password);
+
+
+        return user;
+    }
+
+
+    @Override
+    public List<User> searchUsers(String pattern) {
+        Log.info("searchUsers : pattern = " + pattern);
+
+        List<User> res = new ArrayList<>();
+        String LowerPattern = pattern.toLowerCase();
+        for (Map.Entry<String, User> set : users.entrySet()) {
+            if (set.getValue().getFullName().toLowerCase().contains(LowerPattern)) {
+                res.add(set.getValue());
+            }
+        }
+
+        return res;
+    }
+
+    private URI getServiceURI(String serviceName) {
+
+        URI uri = null;
+        discv.listener();
+        int tries = 0;
+        while (tries < 10) {
+            if (discv.knownUrisOf(serviceName).length > 0) {
+                uri = discv.knownUrisOf(serviceName)[0];
+                break;
+            }
+        }
+        return uri;
+    }
 
 
 }
